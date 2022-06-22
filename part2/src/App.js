3187ce0b7814144ba904f5f42f0d900ddc16325d
RB-bit <rodrigobossi097@gmail.com>
2022-06-22 09:28:45 -0300

    exercises part 2-1

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,39 +1,60 @@
 import './App.css';
+import {Course} from './Course.js';
 
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
+
 
 const App = () => {
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
         {
-        notes.map((note)=>{
-          return <li>{note.content}</li>})
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
         }
-      </ul>
-    </div>
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    courses.map((course) => (
+      <Course key={course.id} course={course} />
+    ))
   )
 }
 
