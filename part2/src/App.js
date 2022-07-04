5eded1d77494699d91687b7b2849200591f47dcc
RB-bit <rodrigobossi097@gmail.com>
2022-07-04 09:03:17 -0300

    finish practice 2.6 - 2.10 | part 2B

--- part2/src/App.js
+++ part2/src/App.js
@@ -4,9 +4,6 @@ import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
-
-
-
 const App = () => {
 
   const [persons, setPersons] = useState([
